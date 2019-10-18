0166f76ebf6ca98b1def8ba56fe76bf985eff40d
Lucas Barros <lucas.lb@samsung.com>
2019-10-18 17:42:38 -0400

    Creating models and relations and setting admin django page

--- radioInterpreter/admin.py
+++ radioInterpreter/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Theme, Key, RadioKey
+
 
 # Register your models here.
+class ThemeDetailAdmin(admin.ModelAdmin):
+    list_display = ['name_theme', 'os']
+
+
+class KeyDetailAdmin(admin.ModelAdmin):
+    list_display = ['name_key']
+
+
+class RadioKeyDetailAdmin(admin.ModelAdmin):
+    model = Theme
+    # TODO: Preciso exibir todas as keys em uma outra coluna
+    # TODO: Exibir os nomes das keys na hora de cadastrar
+    # TODO: Exibir os nomes dos themes na hora de cadastrar
+    list_display = ['get_name']
+    fields = ['theme', 'keys']
+
+    def get_name(self, obj):
+        return obj.theme.name_theme
+
+    get_name.admin_order_field = 'theme'  # Allows column order sorting
+    get_name.short_description = 'Theme Name'  # Renames column head
+
+
+admin.site.register(Theme, ThemeDetailAdmin)
+admin.site.register(Key, KeyDetailAdmin)
+admin.site.register(RadioKey, RadioKeyDetailAdmin)
--- radioInterpreter/models.py
+++ radioInterpreter/models.py
@@ -1,3 +1,47 @@
-from django.db import models
+from django.db import models as modeldjango
+from djongo import models
 
 # Create your models here.
+
+
+OS_IN_MODELS = [
+    ('NN', 'Android 7.0'),
+    ('OO', 'Android 8.0'),
+    ('PP', 'Android 9.0'),
+    ('QQ', 'Android 10.0')
+]
+
+
+class Theme(models.Model):
+    """
+    This model is going to register all problems with a OS associated.
+    It will be use to match key strings with problems.
+    """
+    name_theme = models.CharField(max_length=60)
+    os = models.CharField(max_length=2, choices=OS_IN_MODELS)
+
+    def __str__(self):
+        return self.name_theme
+
+    class Meta:
+        ordering = ['name_theme']
+
+
+class Key(models.Model):
+    """
+    This model is to register all the strings in a list associated with a theme.
+    Ideally, we should have just one Keys(Model) for each Theme(Model), but we can have more.
+    For that reason, this model is many-to-one relationship.
+    """
+    name_key = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name_key
+
+
+class RadioKey(models.Model):
+    # TODO: A adição de keys não está bacana
+    keys = models.ManyToManyField(Key)
+    theme = models.ForeignKey(Theme, on_delete=models.CASCADE)
+
+
